a9978c925e7f3378e567c7312c0c2e36b06d8746
thescorpion1011 <49870353+thescorpion1011@users.noreply.github.com>
2026-09-23 22:45:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,3 @@
 import streamlit as st
 
-API_KEY = st.secrets[27a54483f23214c83ccb6665cdb1f7fb]
-import os
-
-load_dotenv()
-
-API_KEY = os.getenv("API_KEY")
-
-print("API_KEY=", API_KEY)
+API_KEY = st.secrets["OPENWEATHER_API_KEY"]
